8f1cdadce53fd681a453ed07374d5e9828de60f7
Aerika43 <aerikatalaviya@gmail.com>
2024-07-17 14:20:49 +0530

    String

---
+++ Strings.java
@@ -0,0 +1,19 @@
+package String;
+//String  == collection of characters == word
+public class Strings {
+    public static void main(String[] args) {
+        String a = "India is the best";
+        String b = "not";
+        System.out.println("Length is: "+a.length());
+        System.out.println("Lowercase is: "+a.toLowerCase());
+        System.out.println("Uppercase is: "+a.toUpperCase());
+        System.out.println("Index of: "+a.indexOf("i"));
+        System.out.println("Last Index of: "+a.charAt(4));
+        System.out.println("Substring: "+a.substring(3));
+        System.out.println("Substring: "+a.substring(2, 7));
+        System.out.println("Compare: "+b.equals("Not"));
+        System.out.println("Compare: "+b.equals("not"));
+        System.out.println("Ignore Case: "+b.equalsIgnoreCase("Not"));
+        System.out.println("Replace: "+a.replace("India", "Japan"));    
+    }
+}
